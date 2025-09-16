6e667190d20832e853bd05991fcc126788a6c41a
vipin10100001 <75900544+vipin10100001@users.noreply.github.com>
2025-09-16 22:30:46 +0530

    adding attribute to react element say adding attribute id:para of p2
    const p2=React.createElement("p",{id: "para"},"This is a paragraph with id");

--- app.js
+++ app.js
@@ -5,3 +5,7 @@ root.render(heading);
 const topic=React.createElement("p",{},"This is a paragraph");
 const root2=ReactDOM.createRoot(document.getElementById("root2"));
 root2.render(topic);
+
+const p2=React.createElement("p",{id: "para"},"This is a paragraph with id");
+const root3=ReactDOM.createRoot(document.getElementById("root3"));
+root3.render(p2);
