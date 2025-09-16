63542b39bdccbb3411618da9f65dd0d65b011c53
vipin10100001 <75900544+vipin10100001@users.noreply.github.com>
2025-09-16 18:51:46 +0530

    putting all react js code into app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+const heading= React.createElement("h1",{},"Namaste React from Javascript");
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+
+const topic=React.createElement("p",{},"This is a paragraph");
+const root2=ReactDOM.createRoot(document.getElementById("root2"));
+root2.render(topic);
