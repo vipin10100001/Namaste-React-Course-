de98761dfb1241f30cf3fe1b8b2547226c49a4ff
vipin10100001 <75900544+vipin10100001@users.noreply.github.com>
2025-09-18 11:28:32 +0530

    convert html nested div code into react
    // <div id="parent">
    // <div id="child">
    // <h1>I'm h1 Tag
    // </h1>
    // </div>
    // </div>
    // COnverting to react code
    
    const parent=React.createElement("div",{id: "parent"},[React.createElement("div",{id: "child"},[React.createElement(
        "h1",{},"I'm h1 Tag")]),React.createElement( "div",{id: "child"},[React.createElement("h1",{},"I'm h1 Tag")])]);

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const heading= React.createElement("h1",{id: "heading"},"Namaste React from Javascript");
+const heading= React.createElement("h1",{id: "heading",abc:"xyz"},"Namaste React from Javascript");
 const root=ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
 console.log(heading);
@@ -10,3 +10,17 @@ root2.render(topic);
 const p2=React.createElement("p",{id: "para"},"This is a paragraph with id");
 const root3=ReactDOM.createRoot(document.getElementById("root3"));
 root3.render(p2);
+
+// <div id="parent">
+// <div id="child">
+// <h1>I'm h1 Tag
+// </h1>
+// </div>
+// </div>
+// COnverting to react code 
+
+const parent=React.createElement("div",{id: "parent"},[React.createElement("div",{id: "child"},[React.createElement(
+    "h1",{},"I'm h1 Tag")]),React.createElement( "div",{id: "child"},[React.createElement("h1",{},"I'm h1 Tag")])]);
+
+const root4=ReactDOM.createRoot(document.getElementById("root"));
+root4.render(parent);
