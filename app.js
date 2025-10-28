56c919f766b47a9bf02088c27082e8734391e409
vipin10100001 <75900544+vipin10100001@users.noreply.github.com>
2025-10-28 10:40:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,113 +1,24 @@
 
 
-// const heading = React.createElement(
-//     "h1", 
-//     {id:"abc"}, 
-//     "Namaste React from JavaScript"
-//     );
 
-// const root = createRoot(document.getElementById("root"));
-// root.render(heading);
+const Header =() =>{
+  return (
+    <div className="header">
+                       <div className="logo-container">
+                               <img src="https://share.google/images/YBOVBnHABQJoS90qJ"></img>
+                      </div>
 
-
-
-
-// // --- Single heading ---
-// const heading = React.createElement(
-//   "h1",
-//   { id: "heading", abc: "xyz" },
-//   "Namaste React from JavaScript"
-// );
-// const root = createRoot(document.getElementById("root"));
-// root.render(heading);
-
-// // --- Paragraph example ---
-// const topic = React.createElement("p", {}, "This is a paragraph");
-// const root2 = createRoot(document.getElementById("root2"));
-// root2.render(topic);
-
-// // --- Paragraph with ID ---
-// const p2 = React.createElement("p", { id: "para" }, "This is a paragraph with id");
-// const root3 = createRoot(document.getElementById("root3"));
-// root3.render(p2);
-
-// // --- Nested div structure (Example 1) ---
-// const parent = React.createElement(
-//   "div",
-//   { id: "parent" },
-//   [
-//     React.createElement(
-//       "div",
-//       { id: "child", key: "child1" },
-//       [React.createElement("h1", { key: "h1-1" }, "I'm h1 Tag")]
-//     ),
-//     React.createElement(
-//       "div",
-//       { id: "child", key: "child2" },
-//       [React.createElement("h1", { key: "h1-2" }, "I'm h1 Tag")]
-//     ),
-//   ]
-// );
-// const root4 = createRoot(document.getElementById("root4"));
-// root4.render(parent);
-
-// // --- Nested div structure (Example 2) ---
-// const parent2 = React.createElement(
-//   "div",
-//   { id: "parent" },
-//   [
-//     React.createElement(
-//       "div",
-//       { id: "child", key: "child1" },
-//       [
-//         React.createElement("h1", { key: "h1-1" }, "I'm h1x Tag"),
-//         React.createElement("h2", { key: "h2-1" }, "I'm h2 Tag"),
-//       ]
-//     ),
-//     React.createElement(
-//       "div",
-//       { id: "child", key: "child2" },
-//       [
-//         React.createElement("h1", { key: "h1-2" }, "I'm h1 Tag"),
-//         React.createElement("h2", { key: "h2-2" }, "I'm h2 Tag"),
-//       ]
-//     ),
-//   ]
-// );
-// const root5 = createRoot(document.getElementById("root5"));
-// root5.render(parent2);
+                    <div className="nav-items">
+    
+                     </div>
+    </div>
+  )
+}
 
 
-import React from "react";
-import { createRoot } from "react-dom/client";
 
-const heading = <h2 className="heading">Namaste React from JSX directly</h2>;
-
-const Title = () => <h2 className="title">This is a Title</h2>;
-
-const TitleSmall = () => <h6>Title</h6>;
-const elem=<span>YO </span>
-const Card1 = () => (
-  <p className="Card"> {elem} This is a paragraph 1 <br /></p>
-  
-);
-
-const Card2 = () => (
-  <p className="Card">This is a paragraph 2</p>
-);
-
-const Heading = () => (
-  <div className="heading-container">
-    <h2 className="heading">
-      Namaste React from JSX via React Functional Component
-    </h2>
-    <h5>{100 + 200}</h5>
-    <Title />
-    <Card1 />
-    <TitleSmall />
-    <Card2 />
-  </div>
-);
-
-const root = createRoot(document.getElementById("root"));
-root.render(<Heading />);
+const AppLayout=() =>{
+  return (
+    <div className="app"> </div>
+  )
+}
