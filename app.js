8d61e7fecb3de11a76d1f76ea3ab1f76a63d24d2
vipin10100001 <75900544+vipin10100001@users.noreply.github.com>
2025-10-28 11:09:09 +0530

    .

--- app.js
+++ app.js
@@ -1,24 +1,32 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-
-
-const Header =() =>{
+const Header = () => {
   return (
     <div className="header">
-                       <div className="logo-container">
-                               <img src="https://share.google/images/YBOVBnHABQJoS90qJ"></img>
-                      </div>
+      <div className="logo-container">
+        <img src="https://share.google/images/YBOVBnHABQJoS90qJ" alt="logo" />
+      </div>
 
-                    <div className="nav-items">
-    
-                     </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-
-
-const AppLayout=() =>{
+const AppLayout = () => {
   return (
-    <div className="app"> </div>
-  )
-}
+    <div className="app">
+      <Header />
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout />);
