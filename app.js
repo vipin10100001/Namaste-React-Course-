8554f277af5eb2dd91fe021e57e7a2d7f4866d22
vipin10100001 <75900544+vipin10100001@users.noreply.github.com>
2025-11-04 21:08:46 +0530

    .

--- app.js
+++ app.js
@@ -21,49 +21,85 @@ Contact */
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const RestuarantCard=(props) =>{
-  const {resname,photo,cuisine,rating}=props;
-  console.log(props); 
+const RestaurantCard = (props) => {
+  const { resname, cuisine, photo, rating } = props;
   return (
-<div className="res-card">
-        <h3>{props.resname}</h3>
-        <img className="res-logo"  src={props.photo} />
+    <div className="res-card">
+      <h3>{resname}</h3>
+      <img className="res-logo" src={photo} alt={resname} />
+      <h3 style={{ backgroundColor: "red" }}>{cuisine}</h3>
+      <h4>{rating} ⭐</h4>
+      <p>30 Minutes</p>
+    </div>
+  );
+};
 
+const resObj = [
+  {
+    type: "restaurant",
+    data: {
+      resname: "Meghna Food",
+      cuisine: "Biriyani, North Indian, Asian",
+      photo:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRpJivCCXW_14iTjVaacn0ZXVVKG7EMv1Oteg&s",
+      rating: "4.3",
+    },
+  },
+  {
+    type: "restaurant",
+    data: {
+      resname: "Hotel Taj",
+      cuisine: "Chicken Biriyani, North Indian, Asian",
+      photo:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7eQnWra28mt4EcXQRE23Q2s9X-Z1ivWCVPw&s",
+      rating: "4.1",
+    },
+  },
+  {
+    type: "restaurant",
+    data: {
+      resname: "Hotel Taj",
+      cuisine: "Chicken Biriyani, North Indian, Asian",
+      photo:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7eQnWra28mt4EcXQRE23Q2s9X-Z1ivWCVPw&s",
+      rating: "4.1",
+    },
+  },
 
-        <h3 style={{backgroundColor:"red"}}>{props.cuisine}</h3>
-        <h4>{props.rating}</h4>
-        
-        <p>30 Minutes</p>
-         
-</div>
+  {
+    type: "restaurant",
+    data: {
+      resname: "Hotel Taj",
+      cuisine: "Chicken Biriyani, North Indian, Asian",
+      photo:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7eQnWra28mt4EcXQRE23Q2s9X-Z1ivWCVPw&s",
+      rating: "4.1",
+    },
+  },
+];
 
-  )
-}
-
-
-const Body=() =>{
+const Body = () => {
   return (
     <div className="body">
-
-           <div className="Search">Search
-
-                  <div className="res-container">
-                    
-                  <RestuarantCard  resname="Meghna Food" cuisine="Biriyani , North Indian , Asian " photo="https://encrypted-tbn0.gstatic.com/images?q=tbn:AN d9GcRpJivCCXW_14iTjVaacn0ZXVVKG7EMv1Oteg&s" rating="4.3" />
-                  <RestuarantCard  resname="Hotel Taj" cuisine=" Chicken Biriyani , North Indian , Asian " photo ="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7eQnWra28mt4EcXQRE23Q2s9X-Z1ivWCVPw&s" rating ="4.1"/>
-                  
-
-                    </div>
-          </div>
+      <div className="search">Search</div>
+      <div className="res-container">
+        {resObj.map((restaurant, index) => (
+          <RestaurantCard key={index} {...restaurant.data} />
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
 const Header = () => {
   return (
     <div className="header">
       <div className="logo-container">
-        <img   className="logo" src="https://png.pngtree.com/png-vector/20220623/ourmid/pngtree-food-logo-png-image_5296974.png"  />
+        <img
+          className="logo"
+          src="https://png.pngtree.com/png-vector/20220623/ourmid/pngtree-food-logo-png-image_5296974.png"
+          alt="App Logo"
+        />
       </div>
 
       <div className="nav-items">
